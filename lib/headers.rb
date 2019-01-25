c9b95215716271604330b2f90c10a39aa0f90b00
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-01-25 20:22:31 +0100

    Update headers.

--- lib/headers.rb
+++ lib/headers.rb
@@ -1,13 +1,21 @@
 module ReqHeaders
   REQ_HEADER = {
+      'Host' => 'jet.com',
       'accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
       'accept-encoding' => 'gzip, deflate, br',
-      'accept-language' => 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,de-DE;q=0.6,de;q=0.5',
+      'accept-language' => 'en-US;q=0.8,en;q=0.7,de-DE;q=0.6,de;q=0.5',
       'cache-control' => 'max-age=0',
       'upgrade-insecure-requests' => '1',
+      'DNT' => '1',
+      'Connection' => 'keep-alive',
+      'TE' => 'Trailers',
       'user-agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
 
   }
 
 end
+
+
+
+
 
