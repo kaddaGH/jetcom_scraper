5d2e5866465a87b304c96b426114cb395504f185
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-01-25 17:59:43 +0100

    Create scraper.

---
+++ seeder/seeder.rb
@@ -0,0 +1,36 @@
+require 'uri'
+
+pages << {
+    page_type: 'products_listing',
+    method: 'GET',
+    url: "https://jet.com/search?category=15000017",
+    vars: {
+        'input_type' => 'taxonomy',
+        'search_term' => '-',
+        'page' => 1
+    }
+
+
+}
+search_terms = [
+    'Red Bull',
+    'RedBull',
+    'Energy Drink',
+    'Energy Drinks',
+]
+search_terms.each do |search_term|
+
+  pages << {
+      page_type: 'products_listing',
+      method: 'GET',
+      url:'https://jet.com/search?term=' + URI.encode(search_term),
+      vars: {
+          'input_type' => 'search',
+          'search_term' => search_term,
+          'page' => 1
+      }
+
+
+  }
+
+end
