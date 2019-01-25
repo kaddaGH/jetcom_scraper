c1dfcc1dec917a74aab491bada7f48126b9cb78b
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-01-25 18:48:09 +0100

    Update scraper.

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -19,11 +19,13 @@ search_terms = [
     'Energy Drinks',
 ]
 search_terms.each do |search_term|
+  break
 
   pages << {
       page_type: 'products_listing',
       method: 'GET',
       url:'https://jet.com/search?term=' + URI.encode(search_term),
+      ua_type: "gb2",
       vars: {
           'input_type' => 'search',
           'search_term' => search_term,
