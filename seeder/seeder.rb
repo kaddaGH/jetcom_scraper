fd696a4f7908c87a924982a9696416b0e7a76f69
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-01-28 11:34:26 +0100

    Update seeder.

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,23 +1,9 @@
 require 'uri'
 require './lib/headers'
-pages << {
-    page_type: 'products_listing',
-    method: 'GET',
-    url: "https://jet.com/search?category=15000017&zipCode=10011",
-    headers: ReqHeaders::REQ_HEADER,
-    vars: {
-        'input_type' => 'taxonomy',
-        'search_term' => '-',
-        'page' => 1
-    }
 
-
-}
 search_terms = [
-    'Red Bull',
-    'RedBull',
-    'Energy Drink',
-    'Energy Drinks',
+
+    'Energy Drinks'
 ]
 search_terms.each do |search_term|
 
