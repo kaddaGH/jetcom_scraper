14cd36169809e78e35b6e8b3b2d7c6d67c2ec032
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-01-28 03:36:17 +0100

    Update seeder.

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -20,12 +20,11 @@ search_terms = [
     'Energy Drinks',
 ]
 search_terms.each do |search_term|
-  break
 
   pages << {
       page_type: 'products_listing',
       method: 'GET',
-      url:'https://jet.com/search?term='+ URI.encode(search_term),
+      url:'https://jet.com/search?zipCode=10011&term='+ URI.encode(search_term),
       headers: ReqHeaders::REQ_HEADER,
       vars: {
           'input_type' => 'search',
