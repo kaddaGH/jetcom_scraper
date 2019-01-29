c7d41ba27db765bb7874305b840e0955344011c2
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-01-29 10:30:17 +0100

    Update parser.

--- parsers/products_listing.rb
+++ parsers/products_listing.rb
@@ -15,21 +15,20 @@ if data
   products_urls = page['vars']['products_urls']
   if current_page < total_pages
     # get the products
-    rank=0
-    body.css('div.core__Box-avlav9-0.eZsrxv a.BaseProductTile__ItemLink-mors47-0').each do | product|
-      url = 'https://jet.com'+product.attr('href')
-      rank=rank+1
+    rank = 0
+    body.css('div.core__Box-avlav9-0.eZsrxv a.BaseProductTile__ItemLink-mors47-0').each do |product|
+      url = 'https://jet.com' + product.attr('href')
+      rank = rank + 1
       products_urls << {
-          url: url+"&search_term="+page['vars']['search_term']+"&page=#{page['vars']['page']}",
-          vars: {
-              'rank' => rank,
-              'page' => current_page,
-          }
+          'url' => url + "&search_term=" + page['vars']['search_term'] + "&page=#{page['vars']['page']}",
+          'rank' => rank,
+          'page' => current_page
+
       }
     end
 
-     # queue next page
-    url = page['url'].gsub(/page=\d+/,"page=#{current_page+1}")
+    # queue next page
+    url = page['url'].gsub(/page=\d+/, "page=#{current_page + 1}")
     pages << {
         page_type: 'products_listing',
         method: 'GET',
@@ -38,35 +37,45 @@ if data
         vars: {
             'input_type' => page['vars']['input_type'],
             'search_term' => page['vars']['search_term'],
-            'page' => current_page+1,
-            'products_urls'=>products_urls
+            'page' => current_page + 1,
+            'products_urls' => products_urls
         }
     }
 
-  # if it's last page proccess all scraped products urls
+    # if it's last page proccess all scraped products urls
   elsif current_page >= total_pages
     # get the products
-    rank=0
-    body.css('div.core__Box-avlav9-0.eZsrxv a.BaseProductTile__ItemLink-mors47-0').each do | product|
-      url = 'https://jet.com'+product.attr('href')
-      rank=rank+1
+    rank = 0
+    body.css('div.core__Box-avlav9-0.eZsrxv a.BaseProductTile__ItemLink-mors47-0').each do |product|
+      url = 'https://jet.com' + product.attr('href')
+      rank = rank + 1
       products_urls << {
-          url: url+"&search_term="+page['vars']['search_term']+"&page=#{page['vars']['page']}",
-          vars: {
-              'rank' => rank,
-              'page' => current_page,
-          }
+          'url' => url + "&search_term=" + page['vars']['search_term'] + "&page=#{page['vars']['page']}",
+          'rank' => rank,
+          'page' => current_page
       }
     end
+    scrape_url_nbr_products = products_urls.length
+    products_urls.each do |product|
+      pages << {
+          page_type: 'product_details',
+          method: 'GET',
+          url: product['url'] + "&search_term=" + page['vars']['search_term'] + "&page=#{page['vars']['page']}",
+          headers: ReqHeaders::REQ_HEADER,
+          vars: {
+              'input_type' => page['vars']['input_type'],
+              'search_term' => page['vars']['search_term'],
+              'SCRAPE_URL_NBR_PRODUCTS' => scrape_url_nbr_products,
+              'rank' => product['rank'],
+              'page' => product['page']
+          }
+
+      }
 
 
-    puts(products_urls.length )
+    end
 
   end
 
 
-
-
-  end
-
-
+end
